34fa5d49a27e3038f3e66942b956984708050932
Sneha013084 <snehayur30@gmail.com>
2025-10-09 12:08:16 -0400

    task13

--- JSChallenges/task12/script.js
+++ JSChallenges/task12/script.js
@@ -7,6 +7,8 @@ const button = document.getElementById("btn");
 button.addEventListener("click", function() {
 
     heading.textContent = "You clicked the button";
+     heading.style.color = "red"; 
+     heading.style.fontSize = "32px"; 
 
 })
 
