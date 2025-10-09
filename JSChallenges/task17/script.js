bda82cf28c9c71bf0309ed30420827a07ef9edf9
Sneha013084 <snehayur30@gmail.com>
2025-10-09 13:44:08 -0400

    task17

---
+++ JSChallenges/task17/script.js
@@ -0,0 +1,10 @@
+
+const toggleBtn = document.getElementById("toggleBtn")
+
+
+toggleBtn.addEventListener("click", function(){
+    document.body.classList.toggle ("dark-mode");
+        document.body.classList.toggle ("light-mode");
+
+
+})
