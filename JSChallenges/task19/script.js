4b2675d6788ca6fbf928e2e903c63b88a2ed0108
Sneha013084 <snehayur30@gmail.com>
2025-10-09 18:37:54 -0400

    task19

---
+++ JSChallenges/task19/script.js
@@ -0,0 +1,17 @@
+// Use the array `.map()` method and an arrow function to create a new array of the **squares** of these numbers:
+
+// ```js
+// const nums = [1, 2, 3, 4, 5];
+// ```
+
+// Expected result:
+
+// ```js
+// [1, 4, 9, 16, 25]
+// ```
+
+const nums = [1,2,3,4,5];
+
+const doubled = numbs.map(num =>num *2);
+console.log(doubled);
+    
